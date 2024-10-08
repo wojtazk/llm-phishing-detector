c3d9981c5fbbe3bae2baf45e5ce11606868908f9
Wojciech Kowal <w.kowal202@gmail.com>
2024-10-08 13:36:19 +0200

    Add function for loading training data from .csv files

--- helpers.py
+++ helpers.py
@@ -1,5 +1,9 @@
 import torch
 from transformers.modeling_outputs import SequenceClassifierOutput
+import os
+
+import csv
+import sys
 
 
 class OutputColors:
@@ -35,3 +39,48 @@ def get_model_prediction(model_output: SequenceClassifierOutput) -> (int, float)
     [_not_phishing_probability, phishing_probability] = probabilities.tolist()[0]
 
     return predicted_label, phishing_probability
+
+
+def load_evaluation_data() -> (str, int):
+    # increase the csv max field size limit
+    csv.field_size_limit(sys.maxsize)
+
+    eval_data_dir = 'evaluation_data'
+
+    # increase the csv max field size limit
+    csv.field_size_limit(sys.maxsize)
+
+    message_bodies: [str] = []
+    labels: [int] = []
+
+    # get all files in eval_data_dir
+    evaluation_files = os.listdir(eval_data_dir)
+
+    print()
+    for file in evaluation_files:
+        # skip files with extensions other than .csv
+        if not file.endswith('.csv'):
+            continue
+
+        print(f'{OutputColors.BLUE}loading data from: {file}')
+
+        with open(f'{eval_data_dir}/{file}', newline='') as csv_file:
+            try:
+                reader = csv.DictReader(csv_file)
+
+                for row in reader:
+                    body = row['body']
+                    label = row['label']
+
+                    # in case of bad label or body -> skip
+                    if (label is None) or (body is None):
+                        continue
+
+                    # append body and label
+                    message_bodies.append(body)
+                    labels.append(int(label))
+            except csv.Error as err:
+                print(err)
+
+    print(f'{OutputColors.GREEN}loading complete{OutputColors.RESET}')
+    return message_bodies, labels
