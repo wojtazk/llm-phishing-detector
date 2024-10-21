514f370a5583e5c17c31ea9b81d2c4c61ddb6dda
Wojciech Kowal <w.kowal202@gmail.com>
2024-10-21 20:37:03 +0200

    Add multi-line input support in phishing_detector.py

--- phishing_detector.py
+++ phishing_detector.py
@@ -5,14 +5,26 @@ detector = LlmPhishingDetector()
 
 # print device info
 detector.print_device_info()
+
 # get user input
-user_input = input(f'\n{OutputColors.BLUE}Enter potential phishing message (One line only):\n{OutputColors.RESET}')
+inputs = [input(f'{OutputColors.BLUE}\nEnter potential phishing message (type <<EOF to end input):'
+                f'\n{OutputColors.RESET}')]
+
+while True:
+    line = input()
+    if line == '<<EOF':
+        break
+    inputs.append(line)
+
+# join inputs
+user_input = '\n'.join(inputs)
+
 # get model prediction
 predicted_label, predicted_phishing_probability = detector.detect_phishing(user_input)
 
 # print info about the message
 print(f'\nPrediction: '
-      f'{OutputColors.GREEN + 'normal message' if predicted_label == 0 else OutputColors.RED + 'phishing message'}'
+      f'{OutputColors.GREEN + 'normal' if predicted_label == 0 else OutputColors.RED + 'phishing'}'
       f'{OutputColors.RESET}')
 print(f'Phishing probability: {OutputColors.RED if predicted_phishing_probability >= 0.5 else OutputColors.GREEN}'
       f'{predicted_phishing_probability * 100:.5f}%{OutputColors.RESET}')
